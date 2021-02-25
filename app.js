17e3910f6f916e28b8b94f5f81c265430bde934e
Swain Dennis <swain.dennis1@gmail.com>
2021-02-24 20:41:56 -0600

    added js and css

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('hey')
+const container = document.querySelector('#container');
+
+
+
+//function to create 16x16 grid
+function makeRows(rows, cols){
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+
+  for(i = 0; i < (rows * cols); i++){
+    let cell = document.createElement("div");
+    cell.classList.add("grid-item");
+    container.appendChild(cell);
+  };
+  hoverColor();
+};
+
+//function tha creates a random color
+let randomColor = function(){
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for(let i = 0; i < 6; i++){
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+} 
+
+//function that changes colors of divs hovered over
+function hoverColor(){
+  let items = document.querySelectorAll('.grid-item');
+  items.forEach(item => {
+    item.addEventListener('mouseover', () => {
+      item.style.backgroundColor = `${randomColor()}`;
+    })
+  })
+}
+
+makeRows(16, 16);
+
